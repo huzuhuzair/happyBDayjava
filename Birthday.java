e60fefd5db418611c63d82d757b64dc452c0c673
Aamir Bashir <41386062+huzuhuzair@users.noreply.github.com>
2020-08-11 09:54:40 +0530

    Create Birthday.java

---
+++ Birthday.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aamir Bashir
+ */
+public class Birthday
+{
+public static void main(String args[]) throws InterruptedException
+{
+cap y=new cap();
+y.wish();
+}
+}
+class cap
+{
+void wish() throws InterruptedException
+{
+System.out.println("HHHH     HHHH       AA         PPPPPPPPPPP  PPPPPPPPPPP    YY         YY ");
+Thread.sleep(400);
+System.out.println("HHHH     HHHH      AAAA        PP       PP  PP       PP     YY       YY ");     
+Thread.sleep(400);
+System.out.println("HHHH     HHHH     AA  AA       PP       PP  PP       PP      YY     YY  ");
+Thread.sleep(400);
+System.out.println("HHHHHHHHHHHHH    AA    AA      PPPPPPPPPPP  PPPPPPPPPPP       YYYYYYY ");
+Thread.sleep(400);
+System.out.println("HHHH     HHHH   AAAAAAAAAA     PP           PP                    YY ");
+Thread.sleep(400);
+System.out.println("HHHH     HHHH  AA        AA    PP           PP                   YY ");
+Thread.sleep(400);
+System.out.println("HHHH     HHHH AA          AA   PP           PP                  YY ");
+Thread.sleep(400);
+System.out.println("");
+System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RRRRRRRRRRRR  TTTTTTTTTTTTTT HHHH       HHHH  DDDDDDDDDDD        AA   YY          YY ");
+Thread.sleep(400);
+System.out.println("BB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D      AAAA   YY        YY ");  
+Thread.sleep(400);
+System.out.println("BB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D    AA  AA   YY      YY ");
+Thread.sleep(400);
+System.out.println("BBBBBBBBBB        II       RRRRRRRRRRR         TT       HHHHHHHHHHHHHHH  DD          D   AA    AA   YYYYYYYY ");
+Thread.sleep(400);
+System.out.println("BB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D  AAAAAAAAAA       YY ");
+Thread.sleep(400);
+
+System.out.println("BB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D  AA        AA     YY  ");
+Thread.sleep(400);
+System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RR         RR       TT       HHHH       HHHH  DDDDDDDDDDD  AA          AA   YY ");
+Thread.sleep(400);
+System.out.println("                       ~~");
+Thread.sleep(400);
+System.out.println("                       ||");
+Thread.sleep(400);
+System.out.println("                       ||");
+Thread.sleep(400);
+System.out.println("                 ~~~~~~~~~~~~~~~~~   ");                                                                 
+Thread.sleep(400);
+System.out.println("                |                 |");
+Thread.sleep(400);
+System.out.println("                |                 |");
+Thread.sleep(400);
+System.out.println("            ____|                 |___");
+Thread.sleep(400);
+System.out.println("           |     `````````````````    |");
+Thread.sleep(400);
+System.out.println("           |     HAPPY BIRTH DAY      |");
+Thread.sleep(400);
+System.out.println("     ______|                          |_____   ");
+Thread.sleep(400);
+System.out.println("     |      ```````````````````````````     | ");
+Thread.sleep(400);
+System.out.println("     |             11/AUG/2020              | ");
+Thread.sleep(400);
+System.out.println("     |______________________________________|    ");      
+Thread.sleep(400);
+System.out.println("");
+Thread.sleep(400);
+System.out.println("FFFFFFFFFFFF   UU          UU   RRRRRRRRRRRR    UU          UU");
+Thread.sleep(400);
+System.out.println("FF             UU          UU   RR         RR   UU          UU");       
+Thread.sleep(400);
+System.out.println("FF             UU          UU   RR       RR     UU          UU");
+Thread.sleep(400);
+System.out.println("FFFFFFFFFFFF   UU          UU   RRRRRRRRRRR     UU          UU");
+Thread.sleep(400);
+System.out.println("FF             UU          UU   RR       RR     UU          UU");
+Thread.sleep(400);
+System.out.println("FF             UU          UU   RR        RR    UU          UU");
+Thread.sleep(400);
+System.out.println("FF             UUUUUUUUUUUUUU   RR         RR   UUUUUUUUUUUUUU");
+
+}
+}
